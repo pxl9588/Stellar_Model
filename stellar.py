76558f1a1f99be6af90be114613caa501e8c9800
pxl9588 <pxl9588@g.rit.edu>
2018-11-16 13:31:51 -0500

    All routines should return proper things
    There is no inital P or T? always 0

--- stellar.py
+++ stellar.py
@@ -6,7 +6,7 @@ except:
 GAMMA = 5/3
 constants = {"sigma": 5.67051e-5, "c": 2.99792458e+10, "a": 3.826e+33, "G": 5.67051e-5, "k_B": 2.99792458e+10,
                  "m_H": 7.56591e-15, "pi": 3.141592654, "gamma":  GAMMA, "gamrat":  GAMMA / (GAMMA - 1.0),
-                 "kPad":  1.0, "tog_bf": .01, "g_ff": 1.0, "Rsun": 6.9599e+10, "Msun": 1.989e+33, "Lsun": 3.826e+33}
+                 "kPad":  1.0, "g_ff": 1.0, "Rsun": 6.9599e+10, "Msun": 1.989e+33, "Lsun": 3.826e+33}
 diff_eqs = {"dMdr": 0, "dPdr": 0, "dLdr": 0, "dTdr": 0}
 
 def STATSTAR():
@@ -99,6 +99,9 @@ def STATSTAR():
     epslon = [1]
     dlPdlT = [1]
 
+    # took tog_bof out of constants because its dynamic
+    tog_bf = .01
+
     r[0] = initial_cond["Rs"]
     M_r[0] = initial_cond["Ms"]
     L_r[0] = initial_cond["Ls"]
@@ -108,9 +111,12 @@ def STATSTAR():
         rho[0] = 0.0
         kappa[0] = 0.0
         epslon[0] = 0.0
+        print("Pressure or Temperature 0, line 114")
     else:
-        EOS(mass_fractions["X"], mass_fractions["Z"], mass_fractions["XCNO"], mu, P[0], T[0], rho[0], kappa[0],
-            epslon[0], constants["tog_bf"], 1, ierr)
+        [rho[0], kappa[0], epslon[0], tog_bf] = EOS(mass_fractions["X"], mass_fractions["Z"], mass_fractions["XCNO"], mu, P[0], T[0])
+        if ierr == 0:
+            print("Oopsy soupsy")
+            return
 
     constants["kPad"] = 0.3
     irc = 0
@@ -119,10 +125,8 @@ def STATSTAR():
 
     for i in range(0,zone_boundaries["Nstart"]):
         ip1 = i + 1
-        STARTMDL(deltar, mass_fractions["X"], mass_fractions["Z"], mu, initial_cond["Rs"], r[i], M_r[i], L_r[i],
-                 [r[ip1], M_r[ip1], L_r[ip1], T[ip1], P[ip1]], constants["tog_bf"], irc)
-        EOS(mass_fractions["X"], mass_fractions["Z"], mass_fractions["XCNO"], mu, P[ip1], T[ip1], rho[ip1], kappa[ip1],
-            epslon[ip1], constants["tog_bf"], ip1, ierr)
+        [r[ip1], M_r[ip1], L_r[ip1], T[ip1], P[ip1]]= STARTMDL(deltar, mass_fractions["X"], mass_fractions["Z"], mu, initial_cond["Rs"], r[i], M_r[i], L_r[i], tog_bf, irc)
+        [rho[ip1], kappa[ip1], epslon[ip1], tog_bf] = EOS(mass_fractions["X"], mass_fractions["Z"], mass_fractions["XCNO"], mu, P[ip1], T[ip1])
 
         # Determine whether convection will be operating in th next zone
         if i > 1:
@@ -148,7 +152,7 @@ def STATSTAR():
         Nsrtp1 = ip1 + 1
         f_im1 = []
         dfdr = []
-        f_i = []
+        f_i = [0, 0, 0, 0]
 
 
         for i in range(Nsrtp1, zone_boundaries["Nstop"]):
@@ -167,7 +171,7 @@ def STATSTAR():
             dfdr[1] = dMdr(r[im1], rho[im1])
             dfdr[2] = dLdr(r[im1], rho[im1], epslon[im1])
             dfdr[3] = dTdr(r[im1], M_r[im1], L_r[im1], T[im1], rho[im1], kappa[im1], mu, irc)
-            RUNGE(f_im1, dfdr, f_i, r[im1], deltar, irc, mass_fractions["X"], mass_fractions["Z"], mass_fractions["XCNO"], mu, i, ierr)
+            RUNGE(f_im1, dfdr, f_i, r[im1], deltar, irc, mass_fractions["X"], mass_fractions["Z"], mass_fractions["XCNO"], mu)
 
             # Update stellar parameters for the next zone, including adding dr to the old radius
 
@@ -178,8 +182,7 @@ def STATSTAR():
             T[i] = f_i[3]
 
             # Calculate the density, opacity, and energy generation rate for this zone
-            EOS(mass_fractions["X"], mass_fractions["Z"], mass_fractions["XCNO"], mu, P[i], T[i], rho[i], kappa[i],
-                epslon[i], constants["tog_bf"], i, ierr)
+            [rho[i], kappa[i], epslon[i], tog_bf] = EOS(mass_fractions["X"], mass_fractions["Z"], mass_fractions["XCNO"], mu, P[i], T[i])
 
             # Determine whether convection will be operating in the next zone
 
@@ -282,48 +285,47 @@ def STATSTAR():
             clim = '*'
         else:
             clim = ' '
-        results = [["R [" + i + "]", r[i]], ["Qm", Qm], ["L_r[" + i + "]", L_r[i]], ["T[" + i + "", T[i]],
-                   ["P["+ i + "]", P[i]], ["Rho[" + i + "]", rho[i]], ["Kappa[" + i + "]", kappa[i]],
-                   ["Epsilon[" + i + "]", epslon[i]], ["dlPdlT[" + i + "]", dlPdlT[i]]]
+        results = [["R [" + str(i) + "]", r[i]], ["Qm", Qm], ["L_r[" + str(i) + "]", L_r[i]], ["T[" + str(i) + "", T[i]],
+                   ["P["+ str(i) + "]", P[i]], ["Rho[" + str(i) + "]", rho[i]], ["Kappa[" + str(i) + "]", kappa[i]],
+                   ["Epsilon[" + str(i) + "]", epslon[i]], ["dlPdlT[" + str(i) + "]", dlPdlT[i]]]
         print(tabulate(results))
     print("**** The integration has been completed ****")
 
 # Returns r, M_rip1, L_rip1, T_ip1, P_ip1
-def STARTMDL(deltar, X, Z, mu, Rs, r_i, M_ri, L_ri, irc, returnArray):
-    # r
-    returnArray[0] = r_i + deltar
-    # M_rip1
-    returnArray[1] = M_ri
-    # L_rip1
-    returnArray[2] = L_ri
+def STARTMDL(deltar, X, Z, mu, Rs, r_i, M_ri, L_ri, irc):
+
+    r = r_i + deltar
+    M_rip1 = M_ri
+    L_rip1 = L_ri
 
     if irc == 0:
-        # T_ip1
-        returnArray[3] = constants["G"] * returnArray[1]*mu*constants["m_H"]/(4.25*constants["k_B"])*(1/r - 1/Rs)
+        T_rip1 = constants["G"] * M_rip1*mu*constants["m_H"]/(4.25*constants["k_B"])*(1/r - 1/Rs)
         A_bf = 4.34e+25 * Z * (1.0 + X)/constants["tog_bf"]
         A_ff = 3.68e+22 * constants["g_ff"] * (1.0 - Z) * (1.0 + X)
         Afac = A_bf + A_ff
-        # L_ip1
-        returnArray[4] = sqrt((1.0/4.25)*(16.0/3.0*pi*constants["a"]*constants["c"])*(constants["G"] * returnArray[1]/returnArray[2]) *
-                     (constants["k_B"]/(Afac * mu * constants["m_H"]))) * returnArray[3]**4.25
+
+        P_rip1 = sqrt((1.0/4.25)*(16.0/3.0*pi*constants["a"]*constants["c"])*(constants["G"] * M_rip1/L_rip1) *
+                     (constants["k_B"]/(Afac * mu * constants["m_H"]))) * T_rip1**4.25
 
     # This is the convective approximation
     else:
-        returnArray[3] = constants["G"] * returnArray[1] * mu * constants["m_H"] / constants["k_B"] * (1/returnArray[0] - 1/Rs)/constants["gamrat"]
-        returnArray[4] = constants["kPad"]*returnArray[3]**constants["gamrat"]
+        T_rip1 = constants["G"] * M_rip1 * mu * constants["m_H"] / constants["k_B"] * (1/r - 1/Rs)/constants["gamrat"]
+        P_rip1 = constants["kPad"]*T_rip1**constants["gamrat"]
+
+    return [r, M_rip1, L_rip1, T_rip1, P_rip1]
 
 #calculates the values of density,opacity, guillotine-to-gaunt ration, energy gen rate
 # for a radius r
 
 # instead of passing in ierr going to use the return as an error code. 0 for success.
-# Returns [rho, kappa, epslon]
+# Returns [rho, kappa, epslon, tog_bf]
 def EOS(X, Z, XCNO, mu, P, T):
 
     oneo3 = .333333333333
     twoo3 = .666666666667
 
     # solve for density from the ideal gas law
-    if T < 0 or P < 0:
+    if T <= 0.0 or P <= 0.0:
        print('oops soupy')
        return 1
     Prad = constants['a']*(T**4)/3
@@ -331,7 +333,7 @@ def EOS(X, Z, XCNO, mu, P, T):
     rho = (mu * constants['m_H']/constants['k_B']) * (Pgas/T)
     if rho < 0:
         print('oops soupy')
-        return 1
+        return [0, 0, 0, 0, 1]
     # Calc opacity, including guillatine-to-gaunt factor ratio
     tog_bf = 2.82 * (rho*(1+X))**.2
     k_bf = 4.34E25/tog_bf*Z(1 + X)* rho/(T**3.5)
@@ -343,16 +345,16 @@ def EOS(X, Z, XCNO, mu, P, T):
     #The screening factor for the pp chain is calculated as fpp
 
     T6 = T*1E-6
-    fx = .133 * X * ((3+X)*rho)**.5 / (T6**1.5 + comp_eps)
+    fx = .133 * X * ((3+X)*rho)**.5 /T6**1.5
     fpp = 1 + fx*X
-    psipp = 1 + 1.412E8 * (1/x-1)*exp(-49.98*T6**-oneo3)
+    psipp = 1 + 1.412E8 * (1/X-1)*exp(-49.98*T6**-oneo3)
     Cpp = 1 + .0123*T6*oneo3 + .0109*T6**(-twoo3) - .000149*T6
-    epspp = 2.38E6 * rho * x * X * fpp * psipp * Cpp * T6**(-twoo3) * exp(-33.8*T6**(-oneo3))
+    epspp = 2.38E6 * rho * X * X * fpp * psipp * Cpp * T6**(-twoo3) * exp(-33.8*T6**(-oneo3))
     CCNO = 1 + .0027*T6**oneo3 - .0078 * T6**twoo3 - .000149*T6
     epsCNO = 8.67E27 * rho * X * XCNO * CCNO * T6**(-twoo3) * exp(-152.28 * T6**(-oneo3))
     epslon = epspp + epsCNO
 
-    return [rho, kappa, epslon]
+    return [rho, kappa, epslon, tog_bf]
 
 # 'Hydrostatic equilibrium Pressure Gradient
 def dPdr(r,M_r,rho):
@@ -393,22 +395,22 @@ def RUNGE(f_im1, dfdr, f_i, r_im1, deltar, irc, X, Z, XCNO, mu, izone, ierr):
     # Calculate intermediate derviatives from the fundamental stellar structure equations found in subroutine fundeq.
     for i in range(0,3):
         f_temp[i] = f_im1[i] + dr12*dfdr[i]
-    FUNDEQ(r12, f_temp, df1, irc, X, Z, XCNO, mu, izone, ierr)
+    FUNDEQ(r12, f_temp, df1, irc, X, Z, XCNO, mu)
     for i in range(0, 3):
         f_temp[i] = f_im1[i] + dr12*df1[i]
-    FUNDEQ(r12, f_temp, df2, irc, X, Z, XCNO, mu, izone, ierr)
+    FUNDEQ(r12, f_temp, df2, irc, X, Z, XCNO, mu)
     for i in range(0, 3):
         f_temp[i] = f_im1[i] + deltar*df2[i]
-    FUNDEQ(r_i, f_temp, df3, irc, X, Z, XCNO, mu, izone, ierr)
+    FUNDEQ(r_i, f_temp, df3, irc, X, Z, XCNO, mu)
     for i in range(0, 3):
         f_i[i] = f_im1[i] + dr16*(dfdr[i] + 2*df1[i] + 2.0 * df2[i] + df3[i])
 
-def FUNDEQ(r, f, dfdr, irc, X, Z, XCNO, mu, izone, ierr):
+def FUNDEQ(r, f, dfdr, irc, X, Z, XCNO, mu):
     P = f[0]
     M_r = f[1]
     L_r = f[2]
     T = f[3]
-    [rho, kappa, epslon] = EOS(X, Z, XCNO, mu, P, T, constants["tog_bf"], izone, ierr)
+    [rho, kappa, epslon] = EOS(X, Z, XCNO, mu, P, T)
     dfdr[0] = dPdr(r, M_r, rho)
     dfdr[1] = dMdr(r, rho)
     dfdr[2] = dLdr(r, rho, epslon)
